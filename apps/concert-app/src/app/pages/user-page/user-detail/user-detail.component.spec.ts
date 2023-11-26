43c2c91d2b91b27618c76bee1c35c5d943c57b04
Rogier van der Gaag <rm.vandergaag@student.avans.nl>
2023-11-26 14:18:23 +0100

    Added tests for user-detail and user service

--- apps/concert-app/src/app/pages/user-page/user-detail/user-detail.component.spec.ts
+++ apps/concert-app/src/app/pages/user-page/user-detail/user-detail.component.spec.ts
@@ -1,23 +1,59 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
 import { UserDetailComponent } from './user-detail.component';
+import { UserService } from '../../../shared/services/user/user.service';
+import { User } from '../../../shared/models/user.model';
+import { of } from 'rxjs';
 
 describe('UserDetailComponent', () => {
   let component: UserDetailComponent;
   let fixture: ComponentFixture<UserDetailComponent>;
+  let userService: UserService;
+  let activatedRoute: ActivatedRoute;
+
+  const mockUserService = {
+    getUserById: (id: number) => ({
+      id,
+      name: `Simon`,
+      email: `simon@mail.com`,
+      city: `Breda`,
+      birthday: new Date()
+    } as User),
+    // Other methods if needed
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserDetailComponent ]
-    })
-    .compileComponents();
+      declarations: [UserDetailComponent],
+      providers: [
+        { provide: UserService, useValue: mockUserService },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap: of(convertToParamMap({ id: '1' })) // Simulating route param 'id' as '1'
+          }
+        }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(UserDetailComponent);
     component = fixture.componentInstance;
+    userService = TestBed.inject(UserService);
+    activatedRoute = TestBed.inject(ActivatedRoute);
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should load user details based on route parameter', () => {
+    expect(component.user).toBeTruthy();
+    expect(component.user?.id).toBe(1);
+    expect(component.user?.name).toBe('Simon');
+    expect(component.user?.city).toBe('Breda');
+    expect(component.user?.email).toBe('simon@mail.com');
+  });
+
 });
